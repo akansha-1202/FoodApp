1db55cdb3724b756beddf3ed83abe58176ba476c
akansha-1202 <akanshav325@gmail.com>
2023-09-13 00:56:06 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import RoutingFiles from './components/RoutingFiles';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import FindMoreRecipes from './components/search/FindMoreRecipes';
 
 function App() {
   
@@ -10,7 +9,6 @@ function App() {
     <>
     <Header/>
         <RoutingFiles/>
-        {/* <FindMoreRecipes/> */}
     <Footer/>
     </>
   );
